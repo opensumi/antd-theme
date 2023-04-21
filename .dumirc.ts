b8d7fbeb44a38c1895f90e5bef215627d38546c6
boke <sijie.zsj@alibaba-inc.com>
2023-04-21 14:47:17 +0800

    feat: 同步 antd 文档内容脚本

--- .dumirc.ts
+++ .dumirc.ts
@@ -12,4 +12,8 @@ export default defineConfig({
     logo: '/logo.svg',
     footer: false,
   },
+  locales: [
+    { id: 'zh-CN', name: '中文' },
+    { id: 'en-US', name: 'English' },
+  ],
 });
--- src/Foo/index.md
+++
@@ -1,9 +0,0 @@
-# Foo
-
-This is an example component.
-
-```jsx
-import { Foo } from '@opensumi/antd-theme';
-
-export default () => <Foo title="Hello dumi!" />;
-```
