96b9d8544ab6dbcf12f6dcdb230a18f41b8b87cc
boke <sijie.zsj@antgroup.com>
2023-05-05 19:18:25 +0800

    feat: adapt message&Select component

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,35 @@ export const openSumiAntdTheme: ThemeConfig = {
     Notification: {
       colorBgElevated: 'var(--notifications-background)',
       colorTextHeading: 'var(--notifications-foreground)',
-      colorText: 'var(--notifications-foreground)'
+      colorText: 'var(--notifications-foreground)',
+    },
+    Select: {
+      colorBgContainer: 'var(--kt-select-background)',
+      colorText: 'var(--kt-select-foreground)',
+      colorBorder: 'var(--kt-select-border)',
+      colorPrimaryBgHover: 'var(--kt-selectOption-activeBorder)',
+      colorTextDisabled: 'var(--kt-select-disableForeground)',
+      colorBgContainerDisabled: 'var(--kt-select-disableBackground)',
+
+      // select-dropdown
+      colorBgElevated: 'var(--kt-selectDropdown-background)',
+      controlItemBgHover: 'var(--kt-selectDropdown-hoverBackground)',
+      controlItemBgActive: 'var(--kt-selectDropdown-selectionBackground)',
+      paddingXXS: 0,
+    },
+    Dropdown: {
+      colorBgElevated: 'var(--kt-selectDropdown-background)',
+      colorText: 'var(--kt-selectDropdown-foreground)',
+      controlItemBgHover: 'var(--kt-selectDropdown-hoverBackground)',
+      controlItemBgActive: 'var(--kt-selectDropdown-selectionBackground)',
+      paddingXXS: 0,
+    },
+    Collapse: {
+      colorFillAlter: 'transparent',
+      boxShadow: 'none',
+    },
+    Message: {
+      colorBgElevated: 'var(--notifications-background)',
     }
   },
 };
